838a8f7d25d9b3a3153d6fa04f3abb492288f8cf
genius2plus <genius2plus@gmail.com>
2026-08-05 17:32:43 +0800

    feat: 副版測驗卷獨立抽離、購物車檔名加註日期與各書局最新優惠(含南一爸氣購88折)更新

---
+++ remove_deleted.py
@@ -0,0 +1,10 @@
+﻿import re
+
+with open('index.html', 'r', encoding='utf-8') as f:
+    html = f.read()
+
+# Remove any <tr> row that contains style="opacity:0.4;" and 已刪除
+html = re.sub(r'<tr style=\"opacity:0.4;\">.*?已刪除.*?</tr>\n?', '', html, flags=re.DOTALL)
+
+with open('index.html', 'w', encoding='utf-8') as f:
+    f.write(html)
